f7f503a70d3d3714a65bad84b90f894dd2dac17c
Pub4Game <Pub4Game@users.noreply.github.com>
2016-01-04 00:22:18 +0300

    ooops

--- src/main/java/cn/nukkit/block/WallSign.java
+++ src/main/java/cn/nukkit/block/WallSign.java
@@ -33,7 +33,7 @@ public class WallSign extends SignPost {
                 5,
                 4,
                 3,
-                2,
+                2
         };
         if (type == Level.BLOCK_UPDATE_NORMAL) {
             if (this.getSide(faces[this.meta]).getId() == Item.AIR) {
