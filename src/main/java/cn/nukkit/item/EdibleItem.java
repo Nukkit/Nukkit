d296eb7641ac431ba271f8c329c3d7cbb5c587d9
Creeperface01 <honzaxp01@gmail.com>
2016-01-09 16:30:19 +0100

    Update EdibleItem.java

--- src/main/java/cn/nukkit/item/EdibleItem.java
+++ src/main/java/cn/nukkit/item/EdibleItem.java
@@ -5,18 +5,18 @@ package cn.nukkit.item;
  * Nukkit Project
  */
 public class EdibleItem {
-    public static int getRegainAmount(int id) {
-        return getRegainAmount(id, 0);
+    public static int getRegainFoodLevel(int id) {
+        return getRegainFoodLevel(id, 0);
     }
 
-    public static int getRegainAmount(int id, int meta) {
+    public static int getRegainFoodLevel(int id, int meta) {
         switch (id) {
             case Item.APPLE:
                 return 4;
             case Item.MUSHROOM_STEW:
-                return 10;
+                return 6;
             case Item.BEETROOT_SOUP:
-                return 10;
+                return 5;
             case Item.BREAD:
                 return 5;
             case Item.RAW_PORKCHOP:
@@ -34,15 +34,15 @@ public class EdibleItem {
             case Item.MELON_SLICE:
                 return 2;
             case Item.GOLDEN_APPLE:
-                return 10;
+                return 4;
             case Item.PUMPKIN_PIE:
                 return 8;
             case Item.CARROT:
-                return 4;
+                return 3;
             case Item.POTATO:
                 return 1;
             case Item.BAKED_POTATO:
-                return 6;
+                return 5;
             case Item.COOKIE:
                 return 2;
             case Item.COOKED_FISH:
@@ -62,6 +62,68 @@ public class EdibleItem {
                         return 1;
                     default:
                         return 2;
+                }
+        }
+        return 0;
+    }
+
+    public static double getRegainFoodSaturationLevel(int id) {
+        return getRegainFoodSaturationLevel(id, 0);
+    }
+
+    public static double getRegainFoodSaturationLevel(int id, int meta) {
+        switch (id) {
+            case Item.APPLE:
+                return 2.4;
+            case Item.MUSHROOM_STEW:
+                return 7.2;
+            case Item.BEETROOT_SOUP:
+                return 6.2;
+            case Item.BREAD:
+                return 6;
+            case Item.RAW_PORKCHOP:
+                return 1.8;
+            case Item.COOKED_PORKCHOP:
+                return 2.8;
+            case Item.RAW_BEEF:
+                return 1.8;
+            case Item.STEAK:
+                return 2.8;
+            case Item.COOKED_CHICKEN:
+                return 7.2;
+            case Item.RAW_CHICKEN:
+                return 1.2;
+            case Item.MELON_SLICE:
+                return 1.2;
+            case Item.GOLDEN_APPLE:
+                return 9.6;
+            case Item.PUMPKIN_PIE:
+                return 4.8;
+            case Item.CARROT:
+                return 4.8;
+            case Item.POTATO:
+                return 0.6;
+            case Item.BAKED_POTATO:
+                return 7.2;
+            case Item.COOKIE:
+                return 0.4;
+            case Item.COOKED_FISH:
+                switch (meta) {
+                    case 1:
+                        return 9.6;
+                    default:
+                        return 6;
+                }
+            case Item.RAW_FISH:
+                switch (meta) {
+                    case 1:
+                        return 0.4;
+                    case 2:
+                        return 0.2;
+                    case 3:
+                        return 0.2;
+                    default:
+                        return 0.4;
                 }
         }
         return 0;
