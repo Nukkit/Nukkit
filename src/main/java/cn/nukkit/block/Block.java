3b17650d0581f9dad6027e45955f51272c65dd72
Pub4Game <Pub4Game@users.noreply.github.com>
2016-01-15 05:38:23 +0300

    Update Block.java

--- src/main/java/cn/nukkit/block/Block.java
+++ src/main/java/cn/nukkit/block/Block.java
@@ -413,8 +413,8 @@ public abstract class Block extends Position implements Metadatable, Cloneable {
             list[JUNGLE_WOOD_STAIRS] = JungleWoodStairs.class;
             list[STONE_WALL] = StoneWall.class;
 
-            //list[CARROT_BLOCK] = Carrot.class;
-            //list[POTATO_BLOCK] = Potato.class;
+            list[CARROT_BLOCK] = Carrot.class;
+            list[POTATO_BLOCK] = Potato.class;
             list[ANVIL] = Anvil.class;
 
             list[REDSTONE_BLOCK] = Redstone.class;
