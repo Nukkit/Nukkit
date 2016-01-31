341f37e087bea9236f9b17a7c24fe1a49d2556d2
chaunhan <justinngo159753@gmail.com>
2016-01-31 14:38:12 +0700

    Skull.java
    Skull

---
+++ src/main/java/cn/nukkit/item/Skull.java
@@ -0,0 +1,27 @@
+package cn.nukkit.item;
+
+/**
+ * author: Justin
+ */
+
+import cn.nukkit.block.Block;
+
+public class Skull extends Item{
+    public Skull() {
+        this(0, 1);
+    }
+
+    public Skull(Integer meta) {
+        this(meta, 1);
+    }
+
+    public Skull(Integer meta, int count) {
+        super(SKULL, 0, count, "Skull");
+        this.block = Block.get(Item.SKULL_BLOCK);
+    }
+
+    @Override
+    public int getMaxStackSize() {
+        return 1;
+    }
+}
