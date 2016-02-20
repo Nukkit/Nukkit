5c0077bf23e81b5687d35664a46ffb9d1c993903
Jared Peck <linuzo@kidzzone.ca>
2016-02-20 16:30:07 -0700

    update wool colors

--- src/main/java/cn/nukkit/block/BlockWool.java
+++ src/main/java/cn/nukkit/block/BlockWool.java
@@ -9,6 +9,11 @@ import cn.nukkit.utils.BlockColor;
  * Package cn.nukkit.block in project Nukkit .
  */
 public class BlockWool extends BlockSolid {
+    public static final int WHITE = 0;
+    public static final int ORANGE = 1;
+    public static final int MAGENTA = 2;
+    public static final int LIGHT_BLUE = 3;
+    public static final int YELLOW = 4;
 
     public BlockWool() {
         this(0);
@@ -20,10 +25,13 @@ public class BlockWool extends BlockSolid {
 
     @Override
     public String getName() {
-        return ItemDye.getColorName(meta) + " Wool";
-    }
-
-    @Override
+        String[] names = new String[]{
+                "White Wool",
+                "Orange Wool",
+                "Magenta Wool",
+                "Light Blue Wool",
+        }
+        @Override
     public int getId() {
         return WOOL;
     }
