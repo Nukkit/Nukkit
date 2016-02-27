4391de9dc576ba2ce252c39c89e6b5bbc5fc1c72
MrGenga <mrgenga537@gmail.com>
2016-02-27 15:17:34 +0500

    New function setUnsafeLevel

--- src/main/java/cn/nukkit/item/enchantment/Enchantment.java
+++ src/main/java/cn/nukkit/item/enchantment/Enchantment.java
@@ -134,6 +134,11 @@ public abstract class Enchantment implements Cloneable {
         }
     }
 
+    public Enchantment setUnsafeLevel(int level){ //WARNING: buggy
+        this.level = level;
+        return this;
+    }
+
     public int getId() {
         return id;
     }
