7996f8277aec6b4f8f6855ab2e409297fd9eea97
Creeperface01 <honzaxp01@gmail.com>
2016-04-23 12:45:04 +0200

    Update Player.java

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -1579,8 +1579,6 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
         float foodSaturationLevel = this.namedTag.getFloat("foodSaturationLevel");
         this.foodData = new PlayerFood(this, foodLevel, foodSaturationLevel);
 
-        this.server.addOnlinePlayer(this, false);
-
         PlayerLoginEvent ev;
         this.server.getPluginManager().callEvent(ev = new PlayerLoginEvent(this, "Plugin reason"));
         if (ev.isCancelled()) {
@@ -1589,6 +1587,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
             return;
         }
 
+	this.server.addOnlinePlayer(this, false);
         this.loggedIn = true;
 
         if (this.isCreative()) {
