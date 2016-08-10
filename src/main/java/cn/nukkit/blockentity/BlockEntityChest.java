ca99626658287445b945e2a965954a8b131887ec
Creeperface01 <honzaxp01@gmail.com>
2016-08-10 09:31:53 +0200

    Update BlockEntityChest.java

--- src/main/java/cn/nukkit/blockentity/BlockEntityChest.java
+++ src/main/java/cn/nukkit/blockentity/BlockEntityChest.java
@@ -88,7 +88,7 @@ public class BlockEntityChest extends BlockEntitySpawnable implements InventoryH
             return new ItemBlock(new BlockAir(), 0, 0);
         } else {
             CompoundTag data = (CompoundTag) this.namedTag.getList("Items").get(i);
-            return Item.get(data.getShort("id"), data.getShort("Damage"), data.getByte("Count"));
+            return NBTIO.getItemHelper(data);
         }
     }
 
@@ -96,11 +96,7 @@ public class BlockEntityChest extends BlockEntitySpawnable implements InventoryH
     public void setItem(int index, Item item) {
         int i = this.getSlotIndex(index);
 
-        CompoundTag d = new CompoundTag()
-                .putByte("Count", item.getCount())
-                .putByte("Slot", index)
-                .putShort("id", item.getId())
-                .putShort("Damage", item.getDamage());
+        CompoundTag d = NBTIO.putItemHelper(item, index);
 
         if (item.getId() == Item.AIR || item.getCount() <= 0) {
             if (i >= 0) {
