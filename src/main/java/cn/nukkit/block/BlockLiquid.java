41016868bd177508aa747381beb04abde9613166
onebone <jyc00410@gmail.com>
2016-08-11 13:15:35 +0900

    Removed duplication of update toward one block

--- src/main/java/cn/nukkit/block/BlockLiquid.java
+++ src/main/java/cn/nukkit/block/BlockLiquid.java
@@ -251,7 +251,7 @@ public abstract class BlockLiquid extends BlockTransparent {
                         this.getLevel().setBlock(this, new BlockAir(), true);
                     } else {
                         this.getLevel().setBlock(this, this.getBlock(decay), true);
-                        this.getLevel().scheduleUpdate(this, this.tickRate());
+                        //this.getLevel().scheduleUpdate(this, this.tickRate());
                     }
                 } else if (flag) {
                     //this.getLevel().scheduleUpdate(this, this.tickRate());
@@ -323,7 +323,11 @@ public abstract class BlockLiquid extends BlockTransparent {
             }
 
             this.getLevel().setBlock(block, this.getBlock(newFlowDecay), true);
-            this.getLevel().scheduleUpdate(block, this.tickRate());
+
+            /* Scheduling update is done at Level#setBlock(Position, Block, boolean, boolean)
+             * Updating in this code will result in updating twice toward one block
+             */
+            //this.getLevel().scheduleUpdate(block, this.tickRate());
         }
     }
 
