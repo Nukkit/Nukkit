a86acec56726b1a5a50fdc21a12cca899330ec47
yescallop <s@yescallop.cn>
2016-08-29 17:50:50 +0800

    Small change

--- src/main/java/cn/nukkit/entity/EntityLiving.java
+++ src/main/java/cn/nukkit/entity/EntityLiving.java
@@ -299,7 +299,7 @@ public abstract class EntityLiving extends Entity implements EntityDamageable {
         try {
             Block[] blocks = this.getLineOfSight(maxDistance, 1, transparent);
             Block block = blocks[0];
-            if (block != null && Arrays.binarySearch(transparent, block.getId()) < 0) {
+            if (block != null && transparent != null && Arrays.binarySearch(transparent, block.getId()) < 0) {
                 return block;
             }
         } catch (Exception ignored) {
