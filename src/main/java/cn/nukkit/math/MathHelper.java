48291b51214771c94848cd8826f67956e03c589c
NycuRO <nghergu@yahoo.com>
2016-10-07 15:18:20 +0300

    Update MathHelper.java

--- src/main/java/cn/nukkit/math/MathHelper.java
+++ src/main/java/cn/nukkit/math/MathHelper.java
@@ -1,79 +1,476 @@
 package cn.nukkit.math;
 
 import java.util.Random;
+import java.util.UUID;
 
-public class MathHelper {private static float[] a = new float[65536];
+public class MathHelper
+{
+    public static final float SQRT_2 = sqrt_float(2.0F);
 
-    public static float sqrt(float paramFloat) {
-        return (float) Math.sqrt(paramFloat);
+    /**
+     * A table of sin values computed from 0 (inclusive) to 2*pi (exclusive), with steps of 2*PI / 65536.
+     */
+    private static final float[] SIN_TABLE = new float[65536];
+    private static final Random RANDOM = new Random();
+
+    /**
+     * Though it looks like an array, this is really more like a mapping.  Key (index of this array) is the upper 5 bits
+     * of the result of multiplying a 32-bit unsigned integer by the B(2, 5) De Bruijn sequence 0x077CB531.  Value
+     * (value stored in the array) is the unique index (from the right) of the leftmost one-bit in a 32-bit unsigned
+     * integer that can cause the upper 5 bits to get that value.  Used for highly optimized "find the log-base-2 of
+     * this number" calculations.
+     */
+    private static final int[] MULTIPLY_DE_BRUIJN_BIT_POSITION;
+    private static final double FRAC_BIAS;
+    private static final double[] ASINE_TAB;
+    private static final double[] COS_TAB;
+
+    /**
+     * sin looked up in a table
+     */
+    public static float sin(float value)
+    {
+        return SIN_TABLE[(int)(value * 10430.378F) & 65535];
     }
 
-    public static float sin(float paramFloat) {
-        return a[((int) (paramFloat * 10430.378F) & 0xFFFF)];
+    /**
+     * cos looked up in the sin table with the appropriate offset
+     */
+    public static float cos(float value)
+    {
+        return SIN_TABLE[(int)(value * 10430.378F + 16384.0F) & 65535];
     }
 
-    public static float cos(float paramFloat) {
-        return a[((int) (paramFloat * 10430.378F + 16384.0F) & 0xFFFF)];
+    public static float sqrt_float(float value)
+    {
+        return (float)Math.sqrt((double)value);
     }
 
-    public static int floor(double d0) {
-        int i = (int) d0;
-
-        return d0 < (double) i ? i - 1 : i;
+    public static float sqrt_double(double value)
+    {
+        return (float)Math.sqrt(value);
     }
 
-    public static long floor_double_long(double d) {
-        long l = (long) d;
-        return d >= (double) l ? l : l - 1L;
+    /**
+     * Returns the greatest integer less than or equal to the float argument
+     */
+    public static int floor_float(float value)
+    {
+        int i = (int)value;
+        return value < (float)i ? i - 1 : i;
     }
 
-    public static int abs(int number) {
-        if (number > 0) {
-            return number;
-        } else {
-            return -number;
+    /**
+     * Returns the greatest integer less than or equal to the double argument
+     */
+    public static int floor_double(double value)
+    {
+        int i = (int)value;
+        return value < (double)i ? i - 1 : i;
+    }
+
+    /**
+     * Long version of floor_double
+     */
+    public static long floor_double_long(double value)
+    {
+        long i = (long)value;
+        return value < (double)i ? i - 1L : i;
+    }
+
+    public static float abs(float value)
+    {
+        return value >= 0.0F ? value : -value;
+    }
+
+    /**
+     * Returns the unsigned value of an int.
+     */
+    public static int abs_int(int value)
+    {
+        return value >= 0 ? value : -value;
+    }
+
+    public static int ceiling_float_int(float value)
+    {
+        int i = (int)value;
+        return value > (float)i ? i + 1 : i;
+    }
+
+    public static int ceiling_double_int(double value)
+    {
+        int i = (int)value;
+        return value > (double)i ? i + 1 : i;
+    }
+
+    /**
+     * Returns the value of the first parameter, clamped to be within the lower and upper limits given by the second and
+     * third parameters.
+     */
+    public static int clamp_int(int num, int min, int max)
+    {
+        return num < min ? min : (num > max ? max : num);
+    }
+
+    /**
+     * Returns the value of the first parameter, clamped to be within the lower and upper limits given by the second and
+     * third parameters
+     */
+    public static float clamp_float(float num, float min, float max)
+    {
+        return num < min ? min : (num > max ? max : num);
+    }
+
+    public static double clamp_double(double num, double min, double max)
+    {
+        return num < min ? min : (num > max ? max : num);
+    }
+
+    public static double denormalizeClamp(double lowerBnd, double upperBnd, double slide)
+    {
+        return slide < 0.0D ? lowerBnd : (slide > 1.0D ? upperBnd : lowerBnd + (upperBnd - lowerBnd) * slide);
+    }
+
+    /**
+     * Maximum of the absolute value of two numbers.
+     */
+    public static double abs_max(double p_76132_0_, double p_76132_2_)
+    {
+        if (p_76132_0_ < 0.0D)
+        {
+            p_76132_0_ = -p_76132_0_;
+        }
+
+        if (p_76132_2_ < 0.0D)
+        {
+            p_76132_2_ = -p_76132_2_;
+        }
+
+        return p_76132_0_ > p_76132_2_ ? p_76132_0_ : p_76132_2_;
+    }
+
+    public static int getRandomIntegerInRange(Random random, int minimum, int maximum)
+    {
+        return minimum >= maximum ? minimum : random.nextInt(maximum - minimum + 1) + minimum;
+    }
+
+    public static float randomFloatClamp(Random random, float minimum, float maximum)
+    {
+        return minimum >= maximum ? minimum : random.nextFloat() * (maximum - minimum) + minimum;
+    }
+
+    public static double getRandomDoubleInRange(Random random, double minimum, double maximum)
+    {
+        return minimum >= maximum ? minimum : random.nextDouble() * (maximum - minimum) + minimum;
+    }
+
+    public static double average(long[] values)
+    {
+        long i = 0L;
+
+        for (long j : values)
+        {
+            i += j;
+        }
+
+        return (double)i / (double)values.length;
+    }
+
+    /**
+     * the angle is reduced to an angle between -180 and +180 by mod, and a 360 check
+     */
+    public static float wrapDegrees(float value)
+    {
+        value = value % 360.0F;
+
+        if (value >= 180.0F)
+        {
+            value -= 360.0F;
+        }
+
+        if (value < -180.0F)
+        {
+            value += 360.0F;
+        }
+
+        return value;
+    }
+
+    /**
+     * the angle is reduced to an angle between -180 and +180 by mod, and a 360 check
+     */
+    public static double wrapDegrees(double value)
+    {
+        value = value % 360.0D;
+
+        if (value >= 180.0D)
+        {
+            value -= 360.0D;
+        }
+
+        if (value < -180.0D)
+        {
+            value += 360.0D;
+        }
+
+        return value;
+    }
+
+    /**
+     * Adjust the angle so that his value is in range [-180;180[
+     */
+    public static int clampAngle(int angle)
+    {
+        angle = angle % 360;
+
+        if (angle >= 180)
+        {
+            angle -= 360;
+        }
+
+        if (angle < -180)
+        {
+            angle += 360;
+        }
+
+        return angle;
+    }
+
+    /**
+     * parses the string as integer or returns the second parameter if it fails
+     */
+    public static int parseIntWithDefault(String value, int defaultValue)
+    {
+        try
+        {
+            return Integer.parseInt(value);
+        }
+        catch (Throwable var3)
+        {
+            return defaultValue;
         }
     }
 
     /**
-     * Returns a random number between min and max, inclusive.
-     *
-     * @param random The random number generator.
-     * @param min    The minimum value.
-     * @param max    The maximum value.
-     * @return A random number between min and max, inclusive.
+     * parses the string as integer or returns the second parameter if it fails. this value is capped to par2
      */
-    public static int getRandomNumberInRange(Random random, int min, int max) {
-        return min + random.nextInt(max - min + 1);
+    public static int parseIntWithDefaultAndMax(String value, int defaultValue, int max)
+    {
+        return Math.max(max, parseIntWithDefault(value, defaultValue));
     }
 
-    static {
-        for (int i = 0; i < 65536; i++)
-            a[i] = (float) Math.sin(i * 3.141592653589793D * 2.0D / 65536.0D);
-    }
-
-    public static double max(double first, double second, double third, double fourth) {
-        if (first > second && first > third && first > fourth) {
-            return first;
+    /**
+     * parses the string as double or returns the second parameter if it fails.
+     */
+    public static double parseDoubleWithDefault(String value, double defaultValue)
+    {
+        try
+        {
+            return Double.parseDouble(value);
         }
-        if (second > third && second > fourth) {
-            return second;
+        catch (Throwable var4)
+        {
+            return defaultValue;
         }
-        if (third > fourth) {
-            return third;
+    }
+
+    public static double parseDoubleWithDefaultAndMax(String value, double defaultValue, double max)
+    {
+        return Math.max(max, parseDoubleWithDefault(value, defaultValue));
+    }
+
+    /**
+     * Returns the input value rounded up to the next highest power of two.
+     */
+    public static int roundUpToPowerOfTwo(int value)
+    {
+        int i = value - 1;
+        i = i | i >> 1;
+        i = i | i >> 2;
+        i = i | i >> 4;
+        i = i | i >> 8;
+        i = i | i >> 16;
+        return i + 1;
+    }
+
+    /**
+     * Is the given value a power of two?  (1, 2, 4, 8, 16, ...)
+     */
+    private static boolean isPowerOfTwo(int value)
+    {
+        return value != 0 && (value & value - 1) == 0;
+    }
+
+    /**
+     * Uses a B(2, 5) De Bruijn sequence and a lookup table to efficiently calculate the log-base-two of the given
+     * value.  Optimized for cases where the input value is a power-of-two.  If the input value is not a power-of-two,
+     * then subtract 1 from the return value.
+     */
+    public static int calculateLogBaseTwoDeBruijn(int value)
+    {
+        value = isPowerOfTwo(value) ? value : roundUpToPowerOfTwo(value);
+        return MULTIPLY_DE_BRUIJN_BIT_POSITION[(int)((long)value * 125613361L >> 27) & 31];
+    }
+
+    /**
+     * Efficiently calculates the floor of the base-2 log of an integer value.  This is effectively the index of the
+     * highest bit that is set.  For example, if the number in binary is 0...100101, this will return 5.
+     */
+    public static int calculateLogBaseTwo(int value)
+    {
+        return calculateLogBaseTwoDeBruijn(value) - (isPowerOfTwo(value) ? 0 : 1);
+    }
+
+    /**
+     * Rounds the first parameter up to the next interval of the second parameter.
+     *  
+     * For instance, {@code roundUp(1, 4)} returns 4; {@code roundUp(0, 4)} returns 0; and {@code roundUp(4, 4)} returns
+     * 4.
+     */
+    public static int roundUp(int number, int interval)
+    {
+        if (interval == 0)
+        {
+            return 0;
         }
-        return fourth;
+        else if (number == 0)
+        {
+            return interval;
+        }
+        else
+        {
+            if (number < 0)
+            {
+                interval *= -1;
+            }
+
+            int i = number % interval;
+            return i == 0 ? number : number + interval - i;
+        }
     }
 
-    public static int ceil(float floatNumber) {
-        int truncated = (int) floatNumber;
-        return floatNumber > truncated ? truncated + 1 : truncated;
+    public static UUID getRandomUuid(Random rand)
+    {
+        long i = rand.nextLong() & -61441L | 16384L;
+        long j = rand.nextLong() & 4611686018427387903L | Long.MIN_VALUE;
+        return new UUID(i, j);
     }
 
-    public static int clamp(int check, int min, int max) {
-        return check > max ? max : (check < min ? min : check);
+    /**
+     * Generates a random UUID using the shared random
+     */
+    public static UUID getRandomUUID()
+    {
+        return getRandomUuid(RANDOM);
     }
 
-    private MathHelper() {
+    public static double pct(double p_181160_0_, double p_181160_2_, double p_181160_4_)
+    {
+        return (p_181160_0_ - p_181160_2_) / (p_181160_4_ - p_181160_2_);
+    }
+
+    public static double atan2(double p_181159_0_, double p_181159_2_)
+    {
+        double d0 = p_181159_2_ * p_181159_2_ + p_181159_0_ * p_181159_0_;
+
+        if (Double.isNaN(d0))
+        {
+            return Double.NaN;
+        }
+        else
+        {
+            boolean flag = p_181159_0_ < 0.0D;
+
+            if (flag)
+            {
+                p_181159_0_ = -p_181159_0_;
+            }
+
+            boolean flag1 = p_181159_2_ < 0.0D;
+
+            if (flag1)
+            {
+                p_181159_2_ = -p_181159_2_;
+            }
+
+            boolean flag2 = p_181159_0_ > p_181159_2_;
+
+            if (flag2)
+            {
+                double d1 = p_181159_2_;
+                p_181159_2_ = p_181159_0_;
+                p_181159_0_ = d1;
+            }
+
+            double d9 = fastInvSqrt(d0);
+            p_181159_2_ = p_181159_2_ * d9;
+            p_181159_0_ = p_181159_0_ * d9;
+            double d2 = FRAC_BIAS + p_181159_0_;
+            int i = (int)Double.doubleToRawLongBits(d2);
+            double d3 = ASINE_TAB[i];
+            double d4 = COS_TAB[i];
+            double d5 = d2 - FRAC_BIAS;
+            double d6 = p_181159_0_ * d4 - p_181159_2_ * d5;
+            double d7 = (6.0D + d6 * d6) * d6 * 0.16666666666666666D;
+            double d8 = d3 + d7;
+
+            if (flag2)
+            {
+                d8 = (Math.PI / 2D) - d8;
+            }
+
+            if (flag1)
+            {
+                d8 = Math.PI - d8;
+            }
+
+            if (flag)
+            {
+                d8 = -d8;
+            }
+
+            return d8;
+        }
+    }
+
+    public static double fastInvSqrt(double p_181161_0_)
+    {
+        double d0 = 0.5D * p_181161_0_;
+        long i = Double.doubleToRawLongBits(p_181161_0_);
+        i = 6910469410427058090L - (i >> 1);
+        p_181161_0_ = Double.longBitsToDouble(i);
+        p_181161_0_ = p_181161_0_ * (1.5D - d0 * p_181161_0_ * p_181161_0_);
+        return p_181161_0_;
+    }
+
+    public static int getHash(int p_188208_0_)
+    {
+        p_188208_0_ = p_188208_0_ ^ p_188208_0_ >>> 16;
+        p_188208_0_ = p_188208_0_ * -2048144789;
+        p_188208_0_ = p_188208_0_ ^ p_188208_0_ >>> 13;
+        p_188208_0_ = p_188208_0_ * -1028477387;
+        p_188208_0_ = p_188208_0_ ^ p_188208_0_ >>> 16;
+        return p_188208_0_;
+    }
+
+    static
+    {
+        for (int i = 0; i < 65536; ++i)
+        {
+            SIN_TABLE[i] = (float)Math.sin((double)i * Math.PI * 2.0D / 65536.0D);
+        }
+
+        MULTIPLY_DE_BRUIJN_BIT_POSITION = new int[] {0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9};
+        FRAC_BIAS = Double.longBitsToDouble(4805340802404319232L);
+        ASINE_TAB = new double[257];
+        COS_TAB = new double[257];
+
+        for (int j = 0; j < 257; ++j)
+        {
+            double d0 = (double)j / 256.0D;
+            double d1 = Math.asin(d0);
+            COS_TAB[j] = Math.cos(d1);
+            ASINE_TAB[j] = d1;
+        }
     }
 }
