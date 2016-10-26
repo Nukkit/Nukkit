db842b32c9622fc8ee1c2c24ba7aa085b9498245
NycuRO <nghergu@yahoo.com>
2016-10-26 15:51:01 +0300

    Update BiomeSelector.java

--- src/main/java/cn/nukkit/level/generator/biome/BiomeSelector.java
+++ src/main/java/cn/nukkit/level/generator/biome/BiomeSelector.java
@@ -17,12 +17,12 @@ public class BiomeSelector {
 
     private final Map<Integer, Biome> biomes = new HashMap<>();
 
-    private int[] map = new int[64 * 64];
+    private int[] map = new int[4096];
 
     public BiomeSelector(NukkitRandom random, Biome fallback) {
         this.fallback = fallback;
-        this.temperature = new Simplex(random, 2F, 1F / 8F, 1F / 1024F);
-        this.rainfall = new Simplex(random, 2F, 1F / 8F, 1F / 1024F);
+        this.temperature = new Simplex(random, 2.0D, 0.125D, 9.765625E-4D);
+        this.rainfall = new Simplex(random, 2.0D, 0.125D, 9.765625E-4D);
     }
 
     public int lookup(double temperature, double rainfall) {
@@ -54,32 +54,30 @@ public class BiomeSelector {
     }
 
     public void recalculate() {
-        this.map = new int[64 * 64];
-        for (int i = 0; i < 64; ++i) {
-            for (int j = 0; j < 64; ++j) {
-                this.map[i + (j << 6)] = this.lookup(i / 63d, j / 63d);
+        this.map = new int[4096];
+        for(int i = 0; i < 64; ++i) {
+            for(int j = 0; j < 64; ++j) {
+                this.map[i + (j << 6)] = this.lookup((double)i / 63.0D, (double)j / 63.0D);
             }
         }
     }
 
     public void addBiome(Biome biome) {
-        this.biomes.put(biome.getId(), biome);
+        this.biomes.put(Integer.valueOf(biome.getId()), biome);
     }
 
     public double getTemperature(double x, double z) {
-        return (this.temperature.noise2D(x, z, true) + 1) / 2;
+        return (this.temperature.noise2D(x, z, true) + 1.0D) / 2.0D;
     }
 
     public double getRainfall(double x, double z) {
-        return (this.rainfall.noise2D(x, z, true) + 1) / 2;
+        return (this.rainfall.noise2D(x, z, true) + 1.0D) / 2.0D;
     }
 
     public Biome pickBiome(double x, double z) {
-        int temperature = (int) (this.getTemperature(x, z) * 63);
-        int rainfall = (int) (this.getRainfall(x, z) * 63);
-
+        int temperature = (int)(this.getTemperature(x, z) * 63.0D);
+        int rainfall = (int)(this.getRainfall(x, z) * 63.0D);
         int biomeId = this.map[temperature + (rainfall << 6)];
-        return this.biomes.containsKey(biomeId) ? this.biomes.get(biomeId) : this.fallback;
+        return this.biomes.containsKey(Integer.valueOf(biomeId))?(Biome)this.biomes.get(Integer.valueOf(biomeId)):this.fallback;
     }
-
 }
