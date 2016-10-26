e8dad47e3b24eca2992573913da7dd51a0b1a925
NycuRO <nghergu@yahoo.com>
2016-10-26 21:53:19 +0300

    This biome not exist in generation in mcpe. :o

--- src/main/java/cn/nukkit/level/generator/biome/gen/biomes/BeachBiome.java
+++ src/main/java/cn/nukkit/level/generator/biome/gen/biomes/BeachBiome.java
@@ -3,6 +3,7 @@ package cn.nukkit.level.generator.biome;
 import cn.nukkit.block.Block;
 import cn.nukkit.block.BlockSand;
 import cn.nukkit.block.BlockSandstone;
+import cn.nukkit.level.generator.populator.PopulatorTallGrass;
 
 /**
  * Author: PeratX, NycuRO
@@ -10,15 +11,27 @@ import cn.nukkit.block.BlockSandstone;
  */
 public class BeachBiome extends SandyBiome {
     public BeachBiome() {
-        //Todo: SugarCane
 
-        this.setElevation(0.0f, 0.025f);
+        this.setElevation(47, 68);
         this.temperature = 0.8f;
         this.rainfall = 0.4f;
+        
+        this.removePopulator(cactus);
+        this.removePopulator(deadbush);
+        
+        PopulatorTallGrass tallGrass = new PopulatorTallGrass();
+        tallGrass.setBaseAmount(3);
+
+        this.addPopulator(tallGrass);
 
         this.setGroundCover(new Block[]{
                 new BlockSand(),
                 new BlockSand(),
+                new BlockSand(),
+                new BlockSand(),
+                new BlockSandstone(),
+                new BlockSandstone(),
+                new BlockSandstone(),
                 new BlockSandstone(),
                 new BlockSandstone(),
                 new BlockSandstone()
