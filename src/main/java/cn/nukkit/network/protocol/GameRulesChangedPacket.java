8302f638b1890cd3f168b0e66f317ee95098833f
boybook <fangyizhou1997@qq.com>
2016-10-28 18:26:57 +0800

    New GameRulesChangedPacket and base RuleData.

---
+++ src/main/java/cn/nukkit/network/protocol/GameRulesChangedPacket.java
@@ -0,0 +1,36 @@
+package cn.nukkit.network.protocol;
+
+import cn.nukkit.entity.data.EntityMetadata;
+import cn.nukkit.utils.Binary;
+import cn.nukkit.utils.RuleData;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class GameRulesChangedPacket extends DataPacket {
+    public static final byte NETWORK_ID = ProtocolInfo.GAME_RULES_CHANGED_PACKET;
+
+    @Override
+    public byte pid() {
+        return NETWORK_ID;
+    }
+
+    public RuleData[] ruleDatas = new RuleData[0];
+
+    @Override
+    public void decode() {
+
+    }
+
+    @Override
+    public void encode() {
+        this.reset();
+        this.putVarInt(this.ruleDatas.length);
+        for (RuleData rule: this.ruleDatas) {
+            this.putString(rule.name);
+            this.putBoolean(rule.unknown1);
+            this.putBoolean(rule.unknown2);
+        }
+    }
+}
