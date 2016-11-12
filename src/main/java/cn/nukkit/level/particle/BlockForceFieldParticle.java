3e1d6565d599d884ffee774b0d2acdf864bf3180
NycuRO <nghergu@yahoo.com>
2016-11-13 00:59:55 +0200

    fix errors

--- src/main/java/cn/nukkit/level/particle/BlockForceFieldParticle.java
+++ src/main/java/cn/nukkit/level/particle/BlockForceFieldParticle.java
@@ -3,8 +3,8 @@ package cn.nukkit.level.particle;
 import cn.nukkit.math.Vector3;
 
 public class BlockForceFieldParticle extends GenericParticle {
-
-    public BlockForceFieldParticle(Vector3 pos, int data = 0) {
+    
+    public BlockForceFieldParticle(Vector3 pos, int data) {
         super(pos, Particle.TYPE_BLOCK_FORCE_FIELD, data);
     }
 
