f8ad04ddf4d12c978b0b128a50ee8437ec8e9d10
yescallop <s@yescallop.cn>
2016-11-19 11:30:15 +0800

    More 256

--- src/main/java/cn/nukkit/level/Explosion.java
+++ src/main/java/cn/nukkit/level/Explosion.java
@@ -89,7 +89,7 @@ public class Explosion {
                             vBlock.x = pointerX >= x ? x : x - 1;
                             vBlock.y = pointerY >= y ? y : y - 1;
                             vBlock.z = pointerZ >= z ? z : z - 1;
-                            if (vBlock.y < 0 || vBlock.y > 127) {
+                            if (vBlock.y < 0 || vBlock.y > 255) {
                                 break;
                             }
                             Block block = this.level.getBlock(vBlock);
