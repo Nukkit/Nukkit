c4eb2dcc3513510dbc6a90527f0b889ca57fd900
yescallop <s@yescallop.cn>
2016-12-04 11:39:05 +0800

    Fix IllegalArgumentException

--- src/main/java/cn/nukkit/level/format/generic/ChunkConverter.java
+++ src/main/java/cn/nukkit/level/format/generic/ChunkConverter.java
@@ -19,7 +19,7 @@ public class ChunkConverter {
     }
 
     public ChunkConverter from(BaseFullChunk chunk) {
-        if (!(provider instanceof cn.nukkit.level.format.mcregion.Chunk) && !(provider instanceof cn.nukkit.level.format.leveldb.Chunk)) {
+        if (!(chunk instanceof cn.nukkit.level.format.mcregion.Chunk) && !(chunk instanceof cn.nukkit.level.format.leveldb.Chunk)) {
             throw new IllegalArgumentException("From type can be only McRegion or LevelDB");
         }
         this.chunk = chunk;
