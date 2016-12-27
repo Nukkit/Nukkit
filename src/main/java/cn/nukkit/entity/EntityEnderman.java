6bad7e37198ee322533786a5848dbb925d650fb0
PikyCZ <jarda.pikart1999@gmail.com>
2016-12-27 16:10:38 +0100

    Add new mob

---
+++ src/main/java/cn/nukkit/entity/EntityEnderman.java
@@ -0,0 +1,56 @@
+package cn.nukkit.entity;
+
+import cn.nukkit.inventory.InvetoryHolder;
+import cn.nukkit.item.Item;
+import cn.nukkit.nbt.tag.CompoundTag;
+        
+/**
+ * Author: PikyCZ
+ * Nukkit Project
+ * Todo: Width,Lenght,Height
+ */
+        
+public class EntityEnderman extends EntityCreature{
+    
+ public static final int NETWORK_ID = 38;
+    
+ public EntityEnderman(FullChunk chunk,CompoundTag nbt){
+    super(chunk,nbt)
+}
+    
+ @Override
+ public int getNetworkId(){
+     return NETWORK_ID;
+ }
+
+ @Override
+ public float getWidth(){
+     return ?.?f;
+ }
+
+ @Override
+ public float getLenght(){
+     return ?.?f;
+ }
+
+ @Override
+ public float getHeight(){
+     return ?.?f;
+ }
+
+ @Override
+ public String getName(){
+     return this.getName Tag();
+ }
+
+ @Override
+ public item[] getDrops(){
+     return new Item[]{item.get(377,0,1)};
+ }
+
+ @Override
+ public void initEntity(){
+     super.initEntity();
+     setMaxHealt(7);
+ }
+}
