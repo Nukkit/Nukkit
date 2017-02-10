ebcec811b9ab7c21fea63fcf58f5905c88628908
nightmare3832 <nightmare3832@yahoo.co.jp>
2017-02-10 16:58:08 +0900

    Miss

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -94,5 +94,5 @@ public interface ProtocolInfo {
     byte RESOURCE_PACK_DATA_INFO_PACKET = 0x4f;//79
     byte RESOURCE_PACK_CHUNK_DATA_PACKET = 0x50;//80
     byte RESOURCE_PACK_CHUNK_REQUEST_PACKET = 0x51;//81
-    byte TRANSFER_PACKET = 0x51;//81
+    byte TRANSFER_PACKET = 0x52;//82
 }
