1a45c1a6b0f9a545a9385b0e93729fbdd2cb2e0f
NycuRO <nghergu@yahoo.com>
2017-02-10 15:06:20 +0200

    Create PlayerTransferEvent.java

---
+++ src/main/java/cn/nukkit/event/player/PlayerTransferEvent.java
@@ -0,0 +1,37 @@
+package cn.nukkit.event.player;
+
+import cn.nukkit.event.Cancellable;
+import cn.nukkit.event.HandlerList;
+import cn.nukkit.Player;
+
+public class PlayerTransferEvent extends PlayerEvent implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    
+    public static HandlerList getHandlers() {
+        return handlers;
+    }
+    
+    private String address;
+    private int port;
+    
+    public PlayerTransferEvent(Player player, String address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+    
+    public void getAddress() {
+        return this.address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public void getPort() {
+        return this.port;
+    }
+    
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
