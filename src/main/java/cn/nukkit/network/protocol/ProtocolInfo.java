0238f6e077a3fe8783643195fcccfca732ad670d
Scallop Ye <862578730@qq.com>
2017-02-10 08:59:53 +0800

    Release

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -10,8 +10,8 @@ public interface ProtocolInfo {
      * Actual Minecraft: PE protocol version
      */
     byte CURRENT_PROTOCOL = 101;
-    String MINECRAFT_VERSION = "v1.0.3.0";
-    String MINECRAFT_VERSION_NETWORK = "1.0.3.0";
+    String MINECRAFT_VERSION = "v1.0.3";
+    String MINECRAFT_VERSION_NETWORK = "1.0.3";
 
     byte LOGIN_PACKET = 0x01;
     byte PLAY_STATUS_PACKET = 0x02;
