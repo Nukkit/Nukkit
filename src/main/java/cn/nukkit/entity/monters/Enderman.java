06c011190a16c77b0b2f687cc96ffa411a90ede2
PikyCZ <jarda.pikart1999@gmail.com>
2017-02-23 15:47:17 +0100

    Mobs for nukkit

---
+++ src/main/java/cn/nukkit/entity/monters/Enderman.java
@@ -0,0 +1,49 @@
+package cn.nukkit.entities.monsters;
+
+import cn.nukkit.entity.EntityCreature;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+
+public class Enderman extends EntityCreature {
+
+    public static final int NETWORK_ID = 38;
+
+    public Enderman(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+
+    @Override
+    public int getNetworkId() {
+        return NETWORK_ID;
+    }
+
+    @Override
+    public float getWidth() {
+        return 0.72f;
+    }
+
+    @Override
+    public float getHeight() {
+        return 2.8f;
+    }
+
+    public double getSpeed() {
+        return 1.21;
+    }
+
+    @Override
+    protected void initEntity() {
+        this.setMaxHealth(40);
+        super.initEntity();
+    }
+
+    @Override
+    public Item[] getDrops() {
+       return new Item[]{Item.get(Item.ENDER_PEARL)};
+    }
+
+    public int getKillExperience() {
+        return 5; 
+    }
+}
