88bc9fab3d22a745932a38356481f7bcdf477cd1
PikyCZ <jarda.pikart1999@gmail.com>
2017-02-26 19:33:55 +0100

    implement randomizer to enderman

--- src/main/java/cn/nukkit/entity/monsters/EntityEnderman.java
+++ src/main/java/cn/nukkit/entity/monsters/EntityEnderman.java
@@ -1,9 +1,14 @@
 package cn.nukkit.entity.monsters;
 
 import cn.nukkit.entity.EntityCreature;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.entity.monsters.mobutils.dropsRandomizer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EntityEnderman extends EntityCreature {
 
@@ -38,9 +43,16 @@ public class EntityEnderman extends EntityCreature {
         super.initEntity();
     }
 
-    @Override
+     @Override
     public Item[] getDrops() {
-       return new Item[]{Item.get(Item.ENDER_PEARL)};
+        List<Item> drops = new ArrayList<>();
+        if (this.lastDamageCause instanceof EntityDamageByEntityEvent) {
+            int enderPearls = Utils.rand(0, 2); // drops 0-1 enderpearls
+            for (int i = 0; i < enderPearls; i++) {
+                drops.add(Item.get(Item.ENDER_PEARL, 0, 1));
+            }
+        }
+        return drops.toArray(new Item[drops.size()]);
     }
 
     public int getKillExperience() {
