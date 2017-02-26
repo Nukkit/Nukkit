5295fd7897154a04ea2543928fd14aa4aa0b654c
PikyCZ <jarda.pikart1999@gmail.com>
2017-02-26 19:06:22 +0100

    addd utils for random drops

---
+++ src/main/java/cn/nukkit/entity/monsters/MobUtils.java
@@ -0,0 +1,33 @@
+package cn.nukkit.entity.monsters.MobUtils;
+
+import java.util.Random;
+
+
+public class Utils {
+
+    private static final Random random = new Random(System.currentTimeMillis());
+
+    /**
+     * Returns a random number between min (inkl.) and max (excl.) If you want a number between 1 and 4 (inkl) you need to call rand (1, 5)
+     *
+     * @param min min inklusive value
+     * @param max max exclusive value
+     * @return
+     */
+    public static int rand(int min, int max) {
+        if (min == max) {
+            return max;
+        }
+        return min + random.nextInt(max - min);
+    }
+
+    /**
+     * Returns random boolean
+     *
+     * @return a boolean random value either <code>true</code> or <code>false</code>
+     */
+    public static boolean rand() {
+        return random.nextBoolean();
+    }
+
+}
