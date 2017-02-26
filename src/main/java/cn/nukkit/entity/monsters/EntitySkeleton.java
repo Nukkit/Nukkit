b3e69e33d6ed2a919ecb314aed6974273eaa7086
PikyCZ <jarda.pikart1999@gmail.com>
2017-02-26 20:29:25 +0100

    implement Drops to Skeleton

--- src/main/java/cn/nukkit/entity/monsters/EntitySkeleton.java
+++ src/main/java/cn/nukkit/entity/monsters/EntitySkeleton.java
@@ -2,12 +2,17 @@ package cn.nukkit.entity.monsters;
 
 import cn.nukkit.Player;
 import cn.nukkit.entity.EntityCreature;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemBow;
 import cn.nukkit.level.Level;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.network.protocol.MobEquipmentPacket;
+import cn.nukkit.entity.monsters.mobutils.Drops;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EntitySkeleton extends EntityCreature {
 
@@ -64,9 +69,20 @@ public class EntitySkeleton extends EntityCreature {
         return hasUpdate;
     }
 
-    @Override
+     @Override
     public Item[] getDrops() {
-        return new Item[]{Item.get(Item.BONE), Item.get(Item.ARROW)};               
+        List<Item> drops = new ArrayList<>();
+        if (this.lastDamageCause instanceof EntityDamageByEntityEvent) {
+            int bones = Drops.rand(0, 3); // drops 0-2 bones
+            int arrows = Drops.rand(0, 3); // drops 0-2 arrows
+            for (int i = 0; i < bones; i++) {
+                drops.add(Item.get(Item.BONE, 0, 1));
+            }
+            for (int i = 0; i < arrows; i++) {
+                drops.add(Item.get(Item.ARROW, 0, 1));
+            }
+        }
+        return drops.toArray(new Item[drops.size()]);
     }
 
     public int getKillExperience() {
