b263db52c2ada9d747d1fceb9a1c72cd1b391422
AnkitM252 <ankitmaharjankath@gmail.com>
2017-02-27 20:02:42 +0545

    Transfer Event

---
+++ src/main/java/cn/nukkit/command/defaults/TransferCommand.java
@@ -0,0 +1,49 @@
+package cn.nukkit.command.defaults;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class TransferCommand extends VanillaCommand {
+
+    public TransferCommand(String name) {
+        super(name, "%nukkit.command.transfer.description", "%nukkit.command.transfer.usage");
+        this.setPermission("nukkit.command.transfer");
+        this.commandParameters.clear();
+        this.commandParameters.put("1arg",
+                new CommandParameter[]{
+                        new CommandParameter("address", CommandParameter.ARG_TYPE_STRING, false),
+                });
+		this.commandParameters("2arg",
+				new CommandParameter[]{
+					new CommandParameter("port", CommandParameter.ARG_TYPE_INT, false);
+				});
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage(new TranslationContainer("commands.generic.usage", this.usageMessage));
+            
+            return false;
+        }
+        
+		if(!(sender instanceof Player)){
+			sender.sendMessage(TextFormat.RED + "You must run this command as player!");
+		}
+		
+        if (args.length < 2) {
+            sender.sendMessage(new TranslationContainer("commands.generic.usage", this.usageMessage));
+            
+            return false;
+        }
+		
+		sender.transfer(address, port);
+		
+		return true;
+    }
+}
