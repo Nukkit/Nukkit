6ad4300385aaa04d0974f39881aba97d96e47248
PikyCZ <jarda.pikart1999@gmail.com>
2017-03-18 21:49:26 +0100

    Add SkeletonHorse

---
+++ src/main/java/cn/nukkit/entity/passive/EntitySkeletonHorse.java
@@ -0,0 +1,50 @@
+package cn.nukkit.entity.passive;
+
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.entity.mobutils.Drops;
+
+public class EntitySkeletonHorse extends EntityAnimal {
+
+    public static final int NETWORK_ID = 26;
+
+    public EntitySkeletonHorse(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+
+    @Override
+    public int getNetworkId() {
+        return NETWORK_ID;
+    }
+
+    @Override
+    public float getWidth() {
+        return 1.4f;
+    }
+
+    @Override
+    public float getHeight() {
+        return 1.6f;
+    }
+
+    @Override
+    public void initEntity() {
+        super.initEntity();
+        this.setMaxHealth(15);
+    }
+
+    @Override
+    public Item[] getDrops() {
+        if (this.lastDamageCause instanceof EntityDamageByEntityEvent) {
+            return new Item[] { Item.get(Item.BONE, 1, 1) };
+        }
+        return new Item[0];
+    }
+
+    public int getKillExperience() {
+        return Drops.rand(1, 4);
+    }
+
+}
