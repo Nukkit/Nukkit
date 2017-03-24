6d79a0af37415afce88c0b7d00e04a1ef18b2707
PikyCZ <jarda.pikart1999@gmail.com>
2017-03-24 16:45:31 +0100

    update drops for enderman

--- src/main/java/cn/nukkit/entity/monsters/EntityEnderman.java
+++ src/main/java/cn/nukkit/entity/monsters/EntityEnderman.java
@@ -47,7 +47,7 @@ public class EntityEnderman extends EntityCreature {
     public Item[] getDrops() {
         List<Item> drops = new ArrayList<>();
         if (this.lastDamageCause instanceof EntityDamageByEntityEvent) {
-            int EnderPearl = Drops.rand(0, 3); // drops 0-2 Pearls
+            int enderPearl = Drops.rand(0, 2); // drops 0-1 Pearls
             for (int i = 0; i < EnderPearl; i++) {
                 drops.add(Item.get(Item.ENDER_PEARL, 0, 1));
             }
