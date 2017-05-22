482e811357945bd14089e3c49218d72f50a078c5
Creeperface01 <honzaxp01@gmail.com>
2017-05-22 07:33:22 +0200

    Update BlockHopper.java

--- src/main/java/cn/nukkit/block/BlockHopper.java
+++ src/main/java/cn/nukkit/block/BlockHopper.java
@@ -5,6 +5,7 @@ import cn.nukkit.blockentity.BlockEntity;
 import cn.nukkit.blockentity.BlockEntityHopper;
 import cn.nukkit.inventory.ContainerInventory;
 import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemTool;
 import cn.nukkit.level.Level;
 import cn.nukkit.math.BlockFace;
 import cn.nukkit.nbt.tag.CompoundTag;
