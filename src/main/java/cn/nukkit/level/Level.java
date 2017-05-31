4e761792d04944d9f12d2359b9dc0ec6640204fc
kvetinac97 <kvetinac97@users.noreply.github.com>
2017-05-31 19:23:45 +0200

    SpawnChangeEvent is not cancellable -_-

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -2418,16 +2418,13 @@ public class Level implements ChunkManager, Metadatable {
     public void setSpawnLocation(Vector3 pos) {
         Position previousSpawn = this.getSpawnLocation();
         this.provider.setSpawn(pos);
-        SpawnChangeEvent e = new SpawnChangeEvent(this, previousSpawn);
-        this.server.getPluginManager().callEvent(e);
-        if (!e.isCancelled()){
-            SetSpawnPositionPacket pk = new SetSpawnPositionPacket();
-            pk.spawnType = SetSpawnPositionPacket.TYPE_WORLD_SPAWN;
-            pk.x = pos.getFloorX();
-            pk.y = pos.getFloorY();
-            pk.z = pos.getFloorZ();
-            for (Player p : getPlayers().values()) p.dataPacket(pk);
-        }
+        this.server.getPluginManager().callEvent(new SpawnChangeEvent(this, previousSpawn););
+        SetSpawnPositionPacket pk = new SetSpawnPositionPacket();
+        pk.spawnType = SetSpawnPositionPacket.TYPE_WORLD_SPAWN;
+        pk.x = pos.getFloorX();
+        pk.y = pos.getFloorY();
+        pk.z = pos.getFloorZ();
+        for (Player p : getPlayers().values()) p.dataPacket(pk);
     }
 
     public void requestChunk(int x, int z, Player player) {
