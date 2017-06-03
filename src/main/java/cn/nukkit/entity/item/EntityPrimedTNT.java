5cdba60d0f6d24a64b4f309db7e70e7f6d9c920e
Scallop Ye <862578730@qq.com>
2017-06-04 07:02:44 +0800

    TNTPrimeSound

--- src/main/java/cn/nukkit/entity/item/EntityPrimedTNT.java
+++ src/main/java/cn/nukkit/entity/item/EntityPrimedTNT.java
@@ -9,7 +9,7 @@ import cn.nukkit.event.entity.EntityDamageEvent.DamageCause;
 import cn.nukkit.event.entity.EntityExplosionPrimeEvent;
 import cn.nukkit.level.Explosion;
 import cn.nukkit.level.format.FullChunk;
-import cn.nukkit.level.sound.IgniteSound;
+import cn.nukkit.level.sound.TNTPrimeSound;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.network.protocol.AddEntityPacket;
 import cn.nukkit.network.protocol.LevelSoundEventPacket;
@@ -81,7 +81,7 @@ public class EntityPrimedTNT extends Entity implements EntityExplosive {
         this.setDataFlag(DATA_FLAGS, DATA_FLAG_IGNITED, true);
         this.setDataProperty(new IntEntityData(DATA_FUSE_LENGTH, fuse));
 
-        this.level.addSound(new IgniteSound(this));
+        this.level.addSound(new TNTPrimeSound(this));
     }
 
 
