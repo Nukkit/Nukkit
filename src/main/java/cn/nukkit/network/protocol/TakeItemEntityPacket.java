07026d1e6f8293f21a17c119ab94dd143249424d
boybook <fangyizhou1997@qq.com>
2017-06-06 01:05:26 +0800

    Fix pick up item animation.

--- src/main/java/cn/nukkit/network/protocol/TakeItemEntityPacket.java
+++ src/main/java/cn/nukkit/network/protocol/TakeItemEntityPacket.java
@@ -17,8 +17,8 @@ public class TakeItemEntityPacket extends DataPacket {
     @Override
     public void encode() {
         this.reset();
-        this.putVarLong(this.entityId);
         this.putVarLong(this.target);
+        this.putVarLong(this.entityId);
     }
 
     @Override
