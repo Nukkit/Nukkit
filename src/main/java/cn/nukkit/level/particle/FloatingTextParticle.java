e97811b3a957d07d6473c33e00c749c24209211f
boybook <fangyizhou1997@qq.com>
2017-06-05 20:16:23 +0800

    Fix invisible text particle ~

--- src/main/java/cn/nukkit/level/particle/FloatingTextParticle.java
+++ src/main/java/cn/nukkit/level/particle/FloatingTextParticle.java
@@ -81,8 +81,7 @@ public class FloatingTextParticle extends Particle {
             long flags = (
                     (1L << Entity.DATA_FLAG_CAN_SHOW_NAMETAG) |
                     (1L << Entity.DATA_FLAG_ALWAYS_SHOW_NAMETAG) |
-                    (1L << Entity.DATA_FLAG_IMMOBILE) |
-                    (1L << Entity.DATA_FLAG_INVISIBLE)
+                    (1L << Entity.DATA_FLAG_IMMOBILE)
             );
             pk.metadata = new EntityMetadata()
                     .putLong(Entity.DATA_FLAGS, flags)
