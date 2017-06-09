4262bb285be3b69ddc2122f92a6b00dd64ff9dee
Lin Mulan <admin@snake1999.com>
2017-06-09 18:06:42 +0800

    Oops

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -2487,7 +2487,6 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                                 //improved this to take stuff like swimming, ladders, enchanted tools into account, fix wrong tool break time calculations for bad tools (pmmp/PocketMine-MP#211)
                                 //Done by lmlstarqaq
                                 double breakTime = Math.ceil(target.getBreakTime(this.inventory.getItemInHand(), this) * 20);
-                                getServer().getLogger().debug("break time: "+breakTime);
                                 if (breakTime > 0) {
                                     LevelEventPacket pk = new LevelEventPacket();
                                     pk.evid = LevelEventPacket.EVENT_BLOCK_START_BREAK;
