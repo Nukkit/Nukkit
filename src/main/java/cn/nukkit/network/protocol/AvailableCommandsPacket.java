8dd43f4886a33ed5cc28c994d482e7c76135ff98
kvetinac97 <kvetinac97@users.noreply.github.com>
2017-08-24 08:16:48 +0200

    Fix #57 - Commands show now (#63)
    * Fix #57 - Commands show now
    
    * Fix build

--- src/main/java/cn/nukkit/network/protocol/AvailableCommandsPacket.java
+++ src/main/java/cn/nukkit/network/protocol/AvailableCommandsPacket.java
@@ -6,6 +6,7 @@ import cn.nukkit.command.data.CommandParameter;
 import cn.nukkit.utils.BinaryStream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +19,16 @@ public class AvailableCommandsPacket extends DataPacket {
 
     public static final byte NETWORK_ID = ProtocolInfo.AVAILABLE_COMMANDS_PACKET;
     public Map<String, CommandDataVersions> commands;
+    
+    public static final int ARG_FLAG_VALID = 0x100000;
+    public static final int ARG_TYPE_INT       = 0x01;
+    public static final int ARG_TYPE_FLOAT     = 0x02;
+    public static final int ARG_TYPE_VALUE     = 0x03;
+    public static final int ARG_TYPE_TARGET    = 0x04;
+    public static final int ARG_TYPE_STRING    = 0x0c;
+    public static final int ARG_TYPE_POSITION  = 0x0d;
+    public static final int ARG_TYPE_RAWTEXT   = 0x10;
+    public static final int ARG_TYPE_TEXT      = 0x12;
 
     @Override
     public byte pid() {
@@ -28,64 +39,57 @@ public class AvailableCommandsPacket extends DataPacket {
     public void decode() {
     }
 
+    int aliasCommands = 0;
     @Override
-    public void encode() { //TODO: check if this is able to be simplified
+    public void encode() {
         this.reset();
-        List<String> enumValues = new ArrayList<>();
-        Map<String, Integer> enumAdditional = new HashMap<>();
-        List<Object[]> enums = new ArrayList<>();
         BinaryStream commandsStream = new BinaryStream();
-        commands.forEach((name, versions) -> {
-            if (name.equals("help")) { //temp fix for 1.2
-                return;
-            }
-            commandsStream.putString(name);
-            commandsStream.putString(versions.versions.get(0).description);
-            commandsStream.putByte((byte) 0); //flags
-            commandsStream.putByte((byte) 0); //permission level
-            int aliasEnumId;
-            if (versions.versions.get(0).aliases != null && versions.versions.get(0).aliases.length != 0) {
-                List<Integer> aliases = new ArrayList<>();
-                for (String alias : versions.versions.get(0).aliases) {
-                    int targetIndex;
-                    if (!enumAdditional.containsKey(alias)) {
-                        enumValues.add(alias);
-                        enumAdditional.put(alias, enumValues.size() - 1);
-                        targetIndex = enumValues.size() - 1;
-                    } else {
-                        targetIndex = enumAdditional.get(alias);
+        this.commands.forEach((name, versions) -> {
+            if (name.equals("help")) return;
+            ArrayList<String> aliases = new ArrayList<>();
+            aliases.addAll(Arrays.asList(versions.versions.get(0).aliases));
+            aliases.add(name);
+            for (String alias : aliases){
+                commandsStream.putString(alias);
+                commandsStream.putString(versions.versions.get(0).description);
+                commandsStream.putByte((byte) 0);
+                commandsStream.putByte((byte) 0);
+                commandsStream.putLInt(-1);
+                commandsStream.putUnsignedVarInt(versions.versions.get(0).overloads.size());
+                for (CommandOverload overload : versions.versions.get(0).overloads.values()){
+                    commandsStream.putUnsignedVarInt(overload.input.parameters.length);
+                    for (CommandParameter parameter : overload.input.parameters){
+                        commandsStream.putString(parameter.name);
+                        commandsStream.putLInt(ARG_FLAG_VALID | getFlag(parameter.type));
+                        commandsStream.putBoolean(parameter.optional);
                     }
-                    aliases.add(targetIndex);
-                }
-                enums.add(new Object[]{name, aliases});
-                aliasEnumId = enums.size() - 1;
-            } else {
-                aliasEnumId = -1;
-            }
-            commandsStream.putLInt(aliasEnumId);
-            commandsStream.putVarInt(versions.versions.get(0).overloads.size());
-            for (CommandOverload overload : versions.versions.get(0).overloads.values()) {
-                commandsStream.putVarInt(overload.input.parameters.length);
-                for (CommandParameter parameter : overload.input.parameters) {
-                    commandsStream.putString(parameter.name);
-                    commandsStream.putLInt(0);
-                    commandsStream.putBoolean(parameter.optional);
                 }
             }
+            aliasCommands += (aliases.size()-1);
         });
 
-        this.putVarInt(enumValues.size());
-        enumValues.forEach(this::putString);
-        this.putVarInt(0);
-        this.putVarInt(enums.size());
-        enums.forEach(o -> {
-            this.putString(o[0] + "CommandAliases");
-            List<Integer> data = (List<Integer>) o[1];
-            this.putVarInt(data.size());
-            data.forEach(this::putVarInt);
-        });
-
-        this.putVarInt(commands.size());
+        this.putUnsignedVarInt(0);
+        this.putUnsignedVarInt(0);
+        this.putUnsignedVarInt(0);
+        this.putUnsignedVarInt(this.commands.size()+aliasCommands);
         this.put(commandsStream.getBuffer());
+    }
+
+    int getFlag(String type){
+        switch (type){
+            case CommandParameter.ARG_TYPE_BLOCK_POS:
+                return ARG_TYPE_POSITION;
+            case CommandParameter.ARG_TYPE_INT:
+                return ARG_TYPE_INT;
+            case CommandParameter.ARG_TYPE_PLAYER:
+                return ARG_TYPE_TARGET;
+            case CommandParameter.ARG_TYPE_RAW_TEXT:
+                return ARG_TYPE_RAWTEXT;
+            case CommandParameter.ARG_TYPE_STRING:
+            case CommandParameter.ARG_TYPE_STRING_ENUM:
+                return ARG_TYPE_STRING;
+            default:
+                return 0;
+        }
     }
 }
