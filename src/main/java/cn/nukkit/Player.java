5e5f6ce1c6c6954cc939639954680cfef479c8d8
kvetinac97 <kvetinac97@users.noreply.github.com>
2017-09-26 07:17:33 +0200

    Repair BossBar, add old title methods for better compatibility (#19)
    * Make old plugins working on this Nukkit
    
    * Forgot these imports
    
    * Who did invent this?
    
    * Back to how this should be
    
    * Forgot this

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -12,6 +12,7 @@ import cn.nukkit.command.data.CommandDataVersions;
 import cn.nukkit.entity.*;
 import cn.nukkit.entity.data.*;
 import cn.nukkit.entity.item.*;
+import cn.nukkit.entity.mob.EntityCreeper;
 import cn.nukkit.entity.projectile.EntityArrow;
 import cn.nukkit.event.block.ItemFrameDropItemEvent;
 import cn.nukkit.event.block.SignChangeEvent;
@@ -79,6 +80,7 @@ import java.nio.ByteOrder;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * author: MagicDroidX & Box
@@ -3635,6 +3637,22 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
         pk.type = SetTitlePacket.TYPE_RESET;
         this.dataPacket(pk);
     }
+    
+    public void setSubtitle(String subtitle){
+         SetTitlePacket pk = new SetTitlePacket();
+         pk.type = SetTitlePacket.TYPE_SUBTITLE;
+         pk.text = subtitle;
+         this.dataPacket(pk);
+    }
+    
+    public void setTitleAnimationTimes(int fadein, int duration, int fadeout){
+        SetTitlePacket pk = new SetTitlePacket();
+        pk.type = SetTitlePacket.TYPE_ANIMATION_TIMES;
+        pk.fadeInTime = fadein;
+        pk.stayTime = duration;
+        pk.fadeOutTime = fadeout;
+        this.dataPacket(pk);
+    }
 
     public void sendTitle(String title) {
         this.sendTitle(title, "", 20, 20, 5);
@@ -4484,7 +4502,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
      * @return bossBarId  The BossBar ID, you should store it if you want to remove or update the BossBar later
      */
 
-    /*public long createBossBar(String text, int length) {
+    public long createBossBar(String text, int length) {
         // First we spawn a entity
         long bossBarId = 1095216660480L + ThreadLocalRandom.current().nextLong(0, 0x7fffffffL);
         AddEntityPacket pkAdd = new AddEntityPacket();
@@ -4521,11 +4539,11 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
 
         // And now we send the bossbar packet
         BossEventPacket pkBoss = new BossEventPacket();
-        pkBoss.eid = bossBarId;
-        pkBoss.type = BossEventPacket.ADD;
+        pkBoss.bossEid = bossBarId;
+        pkBoss.type = BossEventPacket.TYPE_SHOW;
         this.dataPacket(pkBoss);
         return bossBarId;
-    }*/
+    }
 
     /**
      * Updates a BossBar
@@ -4534,7 +4552,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
      * @param length    The new BossBar length
      * @param bossBarId The BossBar ID
      */
-    /*public void updateBossBar(String text, int length, long bossBarId) {
+    public void updateBossBar(String text, int length, long bossBarId) {
         // First we update the boss bar length
         UpdateAttributesPacket pkAttributes = new UpdateAttributesPacket();
         pkAttributes.entityId = bossBarId;
@@ -4559,11 +4577,11 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
 
         // And now we send the bossbar packet
         BossEventPacket pkBoss = new BossEventPacket();
-        pkBoss.eid = bossBarId;
-        pkBoss.type = BossEventPacket.UPDATE;
+        pkBoss.bossEid = bossBarId;
+        pkBoss.type = BossEventPacket.TYPE_UPDATE;
         this.dataPacket(pkBoss);
         return;
-    }*/
+    }
 
     /**
      * Removes a BossBar
