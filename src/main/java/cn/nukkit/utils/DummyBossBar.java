af4f31167647e714f45fd629daad2ce3403cb9c0
boybook <fangyizhou1997@qq.com>
2017-09-28 00:21:35 +0800

    Oops...

--- src/main/java/cn/nukkit/utils/DummyBossBar.java
+++ src/main/java/cn/nukkit/utils/DummyBossBar.java
@@ -141,7 +141,6 @@ public class DummyBossBar {
                 .putShort(Entity.DATA_AIR, 400)
                 .putShort(Entity.DATA_MAX_AIR, 400)
                 .putLong(Entity.DATA_LEAD_HOLDER_EID, -1)
-                .putFloat(Entity.DATA_SCALE, 1f)
                 .putString(Entity.DATA_NAMETAG, text) // Set the entity name
                 .putInt(Entity.DATA_SCALE, 0); // And make it invisible
 
