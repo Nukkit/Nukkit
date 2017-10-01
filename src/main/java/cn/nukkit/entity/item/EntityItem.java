eb2687f89d40fe5a75568e50cb739e8b2fbe2d94
CreeperFace <honzaxp01@gmail.com>
2017-10-01 15:03:46 +0200

    Fixed client-side item movement handling

--- src/main/java/cn/nukkit/entity/item/EntityItem.java
+++ src/main/java/cn/nukkit/entity/item/EntityItem.java
@@ -100,6 +100,7 @@ public class EntityItem extends Entity {
         }
 
         this.item = NBTIO.getItemHelper(this.namedTag.getCompound("Item"));
+        this.setDataFlag(DATA_FLAGS, DATA_FLAG_IMMOBILE, true);
 
         this.server.getPluginManager().callEvent(new ItemSpawnEvent(this));
     }
