88ca8c705c7178413e0c37db0e84d5e61e3ae5c0
Leonidius20 <leonidslupsky@yandex.ru>
2017-10-02 19:36:19 +0300

    Imports cleanup in BlockObserver.java

--- src/main/java/cn/nukkit/block/BlockObserver.java
+++ src/main/java/cn/nukkit/block/BlockObserver.java
@@ -1,22 +1,9 @@
 package cn.nukkit.block;
 
 import cn.nukkit.Player;
-import cn.nukkit.Server;
-import cn.nukkit.command.Command;
-import cn.nukkit.command.CommandSender;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.Level;
-import cn.nukkit.math.Vector3;
-import cn.nukkit.metadata.BlockMetadataStore;
-import cn.nukkit.metadata.MetadataValue;
-import cn.nukkit.plugin.Plugin;
-import cn.nukkit.plugin.PluginDescription;
-import cn.nukkit.plugin.PluginLoader;
-import cn.nukkit.plugin.PluginLogger;
-import cn.nukkit.utils.Config;
 
-import java.io.File;
-import java.io.InputStream;
 
 /**
  * Created by Leonidius20 on 29.09.17.
