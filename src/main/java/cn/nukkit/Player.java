36a690289a6f1d9ebce1415ea4077bb115018db2
CreeperFace <honzaxp01@gmail.com>
2017-10-05 15:08:09 +0200

    reverted eating sounds

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -2522,8 +2522,8 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                                 break;
                             }
 
-                            this.dataPacket(packet);
-                            Server.broadcastPacket(this.getViewers().values(), packet);
+                            /*this.dataPacket(packet); //bug?
+                            Server.broadcastPacket(this.getViewers().values(), packet);*/
                             break;
                     }
                     break;
