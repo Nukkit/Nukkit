f93e3185820729a15da716ac30d8450d5c189432
CraftYourBukkit <CraftYourBukkit@users.noreply.github.com>
2017-10-24 14:51:14 +0200

    Update RakNetInterface.java

--- src/main/java/cn/nukkit/network/RakNetInterface.java
+++ src/main/java/cn/nukkit/network/RakNetInterface.java
@@ -216,7 +216,7 @@ public class RakNetInterface implements ServerInstance, AdvancedSourceInterface 
                         ProtocolInfo.CURRENT_PROTOCOL + ";" +
                         ProtocolInfo.MINECRAFT_VERSION_NETWORK + ";" +
                         info.getPlayerCount() + ";" +
-                        info.getMaxPlayerCount();
+                        info.getMaxPlayerCount());
     }
 
     public void setPortCheck(boolean value) {
